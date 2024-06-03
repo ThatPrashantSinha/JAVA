7075ccf1ad4c7d0da4e06e7a0d3ea661f5b692a2
Prashant Sinha <prashantsinha9717@gmail.com>
2024-06-04 00:19:24 +0530

    exercise 2

--- exercise2.java
+++ exercise2.java
@@ -3,11 +3,23 @@ public class exercise2 {
         if (kilometersPerHour<0){
             return -1;
         }else{
-            long calcMiles=(long) Math.round(kilometersPerHour*0.6214);
-            return calcMiles;
+            double calcMiles=kilometersPerHour/1.609;
+            return (long)Math.round(calcMiles);
+           
+        }
+        
+    }
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour<0){
+            System.out.println("Invalid Value");
+        }else{
+            int milesPerHour= (int)Math.round(kilometersPerHour/1.609);
+             System.out.println(kilometersPerHour+" km/h = "+ milesPerHour + " mi/h");
         }
     }
     public static void main(String[] args) {
-        System.out.print(toMilesPerHour(75.114));
+        System.out.print(toMilesPerHour(95.75));
+        printConversion(95.75);
+      
     }
 }
