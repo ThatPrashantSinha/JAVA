a00f72302ce42f0c5ff6d1e3fb1943be28e9d309
Prashant Sinha <prashantsinha9717@gmail.com>
2024-06-06 16:08:22 +0530

    exercise11

---
+++ exercise10.java
@@ -0,0 +1,16 @@
+public class exercise10 {
+    public static void printYearsAndDays(long minutes) {
+        if(minutes<0){
+            System.out.println("Invalid Value");
+        }else{
+            long dd=minutes/1440;
+            long yy=dd/365;
+            dd=dd%365;
+            System.out.println(minutes+" min = "+yy+" y and "+dd+" d");
+
+        }
+    }
+    public static void main(String[] args) {
+        printYearsAndDays(561600);
+    }
+}
---
+++ exercise11.java
@@ -0,0 +1,16 @@
+public class exercise11 {
+    public static void printEqual(int numOne,int numTwo,int numThree) {
+        if(numOne<0||numTwo<0||numThree<0){
+            System.out.print( "Invalid Value");
+        }else if((numOne==numThree)&&(numThree==numTwo)){
+            System.out.println( "All numbers are equal");
+        }else if((numOne!=numTwo)&&(numOne!=numThree)&&(numThree!=numTwo)){
+            System.out.println("All numbers are different");
+        }else{
+            System.out.println("Neither all are equal or different");
+        }
+    }
+    public static void main(String[] args) {
+        printEqual(1, 1, 30);
+    }
+}
