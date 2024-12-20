20b2ace22fed0901f0d3087fea531a05e136d774
Prashant Sinha <prashantsinha9717@gmail.com>
2024-12-20 14:01:34 +0530

    blah blah

--- heh.java
+++ heh.java
@@ -1,5 +1,17 @@
 class heh{
-    public static void main(String[] args) {
-        System.out.println("Hi, World");
+    public int[] twoSum(int[] nums, int target) {
+        int temp;
+        int[] ind= new int[2];
+        for(int i=0;i<nums.length;i++){
+            temp=target-nums[i];
+            for(int j=0;i<nums.length;j++){
+                if(nums[j]==target-temp){
+                    ind[0]=i;
+                    ind[1]=i+1;
+                }
+            }
+
+        }
+        return ind;
     }
 }
