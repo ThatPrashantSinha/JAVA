a7adf5c847b697bde60a56b341aac4289bcdada7
Prashant Sinha <prashantsinha9717@gmail.com>
2025-03-04 15:30:08 +0530

    blah blah

--- methods.java
+++ methods.java
@@ -10,10 +10,10 @@ public class methods {
         int daru = 5000;
         int savings =(salary-(rent+expenses+daru));
         System.out.println(savings);
-        calculateSavingsFaster(10000,3000,5000,3000);
+        calculateSavings(10000,3000,5000,3000);
     }
     // or
-    public static void calculateSavingsFaster(int salary,int rent,int expenses,int daru){
+    public static void calculateSavings(int salary,int rent,int expenses,int daru){
          System.out.println(salary-(rent+expenses+daru));
     }
     //method overloading...(creating multiple methods with same name...which accept diff parameters)
